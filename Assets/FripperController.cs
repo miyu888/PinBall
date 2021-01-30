f7db7d12ba7b63ffd2e5be468bec641f8617bfce
miyu888 <55343240+miyu888@users.noreply.github.com>
2021-01-30 15:42:06 +0900

    Initial commit

---
+++ Assets/FripperController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointにコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+    //初期の傾き
+    private float defaultAngle = 20;
+    //はじいた時の傾き
+    private float flickAngle = -20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+ 
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印キーかAキーを押したとき左フリッパーを動かす
+        if((Input.GetKeyDown(KeyCode.LeftArrow)|| Input.GetKeyDown(KeyCode.A)) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //右矢印キーかDキーを押したとき右フリッパーを動かす
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //左矢印キーかAキー離されたとき左フリッパーを元に戻す
+        if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)) && tag == "LeftFripperTag")
+        {
+                SetAngle(this.defaultAngle);
+        }
+
+        //右矢印キーかDキーを押したとき右フリッパーを元に戻す
+        if ((Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)) && tag == "RightFripperTag")
+        {
+                SetAngle(this.defaultAngle);
+        }
+        //下矢印キーかSキーを押したとき同時に左右のフリッパーを動かす
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && tag == "RightFripperTag")
+        {
+                SetAngle(this.flickAngle);
+        }
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        //下矢印キーかSキーを押したとき同時に左右のフリッパーを元に戻す
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+    }
+
+        //フリッパーの傾きを設定
+        public void SetAngle (float angle)
+        {
+            JointSpring jointSpr = this.myHingeJoint.spring;
+            jointSpr.targetPosition = angle;
+            this.myHingeJoint.spring = jointSpr;
+        }
+
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    //スコアオブジェクト取得
+    private GameObject scoreText;
+    public Text scoreLabel;
+    //コンポーネントをいれる
+    private Material myMaterial;
+    private int Score = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+
+        //障害物にアタッチしているマテリアルを取得
+        this.myMaterial = GetComponent<Renderer>().material;
+
+
+        //void AddPoint(int point)
+        //{
+          //  Score += point;
+        //}
+        //if(hp < 0)
+        //{
+         //   FindObjectOfType<Score>().AddPoint();
+        //}
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+  
+    }
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+
+
+        //SmallStarTagは10点
+        //if (tag == "SmallStarTag")←これは自分のTagが””だったら
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            Score += 10;
+        }
+        //LargeStarTagは25点
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            Score += 25;
+        }
+        //SmallCloudTagは40点
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            Score += 40;
+        }
+        //LargeCloudTagは50点
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            Score += 50;
+        }
+
+        //得点を表示
+        this.scoreText.GetComponent<Text>().text = "Score:" + Score;
+    }
+   
+}
